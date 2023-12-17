a29af8bf798d7e22d7beccff0203ee07ab35817b
Patipol Tiyajamorn <53873809+Patipol-BKK@users.noreply.github.com>
2023-12-17 12:09:58 +0700

    Update models.py

--- src/models.py
+++ src/models.py
@@ -49,8 +49,8 @@ class WhisperForLetterClassification(nn.Module):
 				new_embedding.weight.data.copy_(original_embedding_weights[:in_channels, :])
 				module = new_embedding
 
-		for param in self.encoder.parameters():
-			param.requires_grad = False
+		# for param in self.encoder.parameters():
+		# 	param.requires_grad = False
 
 
 	def _parse_modules(self, whisper, modules):
@@ -86,10 +86,11 @@ class WhisperForLetterClassification(nn.Module):
 		pooled_output = self.classifier(pooled_output)
 		return pooled_output
 
-	# def _window_segments(self, x):
-
-
 	# def inference(self, x):
+	# 	window_start = 0
+	# 	window_end = int(self.audio_config.window_length / 10)
+
+
 		
 
 
